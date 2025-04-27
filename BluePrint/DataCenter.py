e4e32400141a23be8ee4939d7b68f4fe1c721e71
GaryLee93 <gary313128@gmail.com>
2025-04-27 21:55:13 +0800

    add arguments of request into function

--- BluePrint/DataCenter.py
+++ BluePrint/DataCenter.py
@@ -4,8 +4,8 @@ DATA_CENTER_BLUEPRINT = Blueprint('dc', __name__)
 
 @DATA_CENTER_BLUEPRINT.route('/', methods=['POST'])
 def AddDC():
-    name = request.json.get('name')
-    default_height = request.json.get('default_height')
+    name = request.json.get('name' , type = str)
+    default_height = request.json.get('default_height', type = int)
     ## write into database
     return 
 
@@ -14,17 +14,23 @@ def GetAllDC():
     return 
 
 @DATA_CENTER_BLUEPRINT.route('/<dc_id>', methods=['PUT'])
-def ModifyDC():
+def ModifyDC(dc_id):
+    name = request.json.get('name', type = str)
+    default_height = request.json.get('default_height', type = int)
     return "Modify Data Center"
 
 @DATA_CENTER_BLUEPRINT.route('/<dc_id>', methods=['DELETE'])
-def DeleteDC():
+def DeleteDC(dc_id):
+    force = request.args.get('force', default=False, type=bool)
     return "Delete Data Center"
 
 @DATA_CENTER_BLUEPRINT.route('/<dc_id>/room', methods=['POST'])
-def AddRoomToDC():
+def AddRoomToDC(dc_id):
+    name = request.json.get('name', type=str)
+    height = request.json.get('height', type=int)
     return "Add Room to Data Center"
 
 @DATA_CENTER_BLUEPRINT.route('/<dc_id>/room/all', methods=['PUT'])
-def GetRoomsByDCID():
+def GetRoomsByDCID(dc_id):
+
     return "Get All Rooms in Data Center"
--- BluePrint/Room.py
+++ BluePrint/Room.py
@@ -3,22 +3,28 @@ from flask import Blueprint, request
 ROOM_BLUEPRINT = Blueprint('room', __name__)
 
 @ROOM_BLUEPRINT.route('/<room_id>', methods=['PUT'])
-def ModifyRoom():
+def ModifyRoom(room_id):
+    name = request.json.get('name', type=str)
+    height = request.json.get('height', type=int)
     return "modify room"
 
 @ROOM_BLUEPRINT.route('/mulitple', methods=['PUT'])
 def ModifyMultipleRoom():
+    rooms_to_modify = request.get_json()
     return "modify multiple room"
 
 @ROOM_BLUEPRINT.route('/<room_id>', methods=['DELETE'])
-def DeleteRoom():
+def DeleteRoom(room_id):
+    force = request.args.get('force', default=False, type=bool)
     return "delete room"
 
 @ROOM_BLUEPRINT.route('/room/<room_id>/rack', methods=['POST'])
-def AddRackToRoom():
+def AddRackToRoom(room_id):
+    name = request.json.get('name', type=str)
+    height = request.json.get('height', type=int)
     return "add rack to room"
 
 @ROOM_BLUEPRINT.route('/room/<room_id>/rack/all', methods=['GET'])
-def GetRacksByRoom():
+def GetRacksByRoom(room_id):
     return "get racks by room"
 
