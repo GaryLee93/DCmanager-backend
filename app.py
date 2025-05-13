262e5b4169346910f9a66922dd8bee81db03e90a
Ptorioo <98724114+Ptorioo@users.noreply.github.com>
2025-05-13 21:59:31 +0800

    fix: register name fix

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from BluePrint import DataCenter, Room, Rack, Host, Service, Auth
+from BluePrint.DataCenter import DATA_CENTER_BLUEPRINT
+from BluePrint.Room import ROOM_BLUEPRINT
+from BluePrint.Rack import RACK_BLUEPRINT
+from BluePrint.Host import HOST_BLUEPRINT
+from BluePrint.Service import SERVICE_BLUEPRINT
+from BluePrint.Auth import AUTH_BLUEPRINT
 
 app = Flask(__name__)
-app.register_blueprint(DataCenter)
-app.register_blueprint(Room)
-app.register_blueprint(Rack)
-app.register_blueprint(Host)
-app.register_blueprint(Service)
-app.register_blueprint(Auth)
+app.register_blueprint(DATA_CENTER_BLUEPRINT, url_prefix="/datacenter")
+app.register_blueprint(ROOM_BLUEPRINT, url_prefix="/room")
+app.register_blueprint(RACK_BLUEPRINT, url_prefix="/rack")
+app.register_blueprint(HOST_BLUEPRINT, url_prefix="/host")
+app.register_blueprint(SERVICE_BLUEPRINT, url_prefix="/service")
+app.register_blueprint(AUTH_BLUEPRINT, url_prefix="/auth")
 
 @app.route("/")
 def hello():
