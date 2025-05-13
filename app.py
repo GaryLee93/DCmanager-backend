87b460eb1070a3fc77d0cdad801d5de29c3a2d11
GaryLee93 <gary313128@gmail.com>
2025-05-13 20:09:36 +0800

    add blueprint into app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from BluePrint import DataCenter, Room, Rack, Host
+from BluePrint import DataCenter, Room, Rack, Host, Service, Auth
 
 app = Flask(__name__)
+app.register_blueprint(DataCenter)
+app.register_blueprint(Room)
+app.register_blueprint(Rack)
+app.register_blueprint(Host)
+app.register_blueprint(Service)
+app.register_blueprint(Auth)
 
 @app.route("/")
 def hello():
