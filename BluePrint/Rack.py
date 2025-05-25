65cab6832e6217feade9b219c72715f0ebc017b8
GaryLee93 <gary313128@gmail.com>
2025-05-25 15:04:00 +0800

    Add param check and service name modification in ModifyRack function

--- BluePrint/Rack.py
+++ BluePrint/Rack.py
@@ -2,7 +2,9 @@ from flask import Blueprint, request, jsonify, Response
 from DataBaseManage import *
 from .Host import DeleteHost, ModifyHost
 from dataclasses import asdict
+from utils.schema import Rack
 
+Service_Manager = ServiceManager()
 Room_Manager = RoomManager()
 Rack_Manager = RackManager()
 RACK_BLUEPRINT = Blueprint("rack", __name__)
@@ -30,7 +32,6 @@ def AddNewRack():
     rack = Rack_Manager.createRack(name, height, room_name)
     return jsonify(asdict(rack)), 200
 
-
 @RACK_BLUEPRINT.route("/<rack_name>", methods=["GET", "PUT", "DELETE"])
 def ProcessRack(rack_name):
     if request.method == 'GET':
@@ -42,7 +43,8 @@ def ProcessRack(rack_name):
         name = data.get('name')
         height = data.get('height')
         room_name = data.get('room_name')
-        return ModifyRack(rack_name, name, height, room_name)
+        service_name = data.get('service_name')
+        return ModifyRack(rack_name, name, height, room_name, service_name)
     return jsonify({"error": "Invalid Method"}), 405
 
 def GetRack(rack_name):
@@ -53,17 +55,18 @@ def GetRack(rack_name):
         return jsonify(asdict(rack)), 200
 
 # database can't update room_id
-def ModifyRack(rack_name, name, height, room_name):
+def ModifyRack(rack_name, new_rack_name, height, room_name, service_name):
     rack = Rack_Manager.getRack(rack_name)
     if rack == None:
         return jsonify({"error": "Rack Not Found"}), 404
-    if room_name and Room_Manager.getRoom(room_name) == None:
-        return jsonify({"error": "Destination Room Not Found"}), 404
-    if not Rack_Manager.updateRack(rack_name, name, height, room_name):
+    ProcessParams(rack, new_rack_name, height, room_name, service_name)
+
+    if not Rack_Manager.updateRack(rack_name, new_rack_name, height, room_name):
         return jsonify({"error": "Rack Update Failed"}), 500
-    for host in rack.hosts:
-        if not ModifyHost(host.name, host.name, host.height, host.running, name, host.pos):
-            return jsonify({"error": "Host Update Failed"}), 500
+    if service_name != rack.service_name:
+        Service_Manager.unassignRackFromService(rack)
+        Service_Manager.assignRackToService(new_rack_name, service_name)
+    UpdateHostsInRack(rack, new_rack_name, service_name)
     return Response(status = 200)
 
 def DeleteRack(rack_name):
@@ -75,3 +78,26 @@ def DeleteRack(rack_name):
     if not Rack_Manager.deleteRack(rack_name):
         return jsonify({"error": "Delete Failed"}), 500
     return Response(status=200)
+
+def ProcessParams(rack: Rack, new_name, height, room_name, service_name):
+    if new_name and Rack_Manager.getRack(new_name) and new_name != rack.name:
+        return jsonify({"error": "Rack Name Already Exists"}), 400
+    elif not new_name:
+        new_name = rack.name
+    if height and height <= 0:
+        return jsonify({"error": "Invalid Rack Height"}), 400
+    elif not height:
+        height = rack.height
+    if room_name and not Room_Manager.getRoom(room_name):
+        return jsonify({"error": "Room Not Found"}), 404
+    elif not room_name:
+        room_name = rack.room_name
+    if service_name and not Service_Manager.getService(service_name):
+        return jsonify({"error": "Service Not Found"}), 404
+    elif not service_name:
+        service_name = rack.service_name
+
+def UpdateHostsInRack(rack: Rack, room_name, service_name):
+    for host in rack.hosts:
+        if not ModifyHost(host.name, host.name, host.height, host.running, room_name, host.pos, service_name):
+            return jsonify({"error": "Host Update Failed"}), 500
