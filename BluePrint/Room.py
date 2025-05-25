b9ccd6fc56e23044e3d83e9b0dda08450a392186
C-W-Z <chenweizhang3021@gmail.com>
2025-05-25 11:20:50 +0800

    fix: update room_name for under racks (to update dc_name of rack)

--- BluePrint/Room.py
+++ BluePrint/Room.py
@@ -51,6 +51,8 @@ def ModifyRoom(room_name, new_name, height, dc_name):
         return jsonify({"error": "Datacenter Not Found"}), 404
     if not Room_Manager.updateRoom(room_name, new_name, height, dc_name):
         return jsonify({"error": "Room Update Failed"}), 500
+    if new_name is None:
+        new_name = room_name
     for rack in room.racks:
         if not ModifyRack(rack.name, rack.name, rack.height, new_name):
             return jsonify({"error": "Rack Update Failed"}), 500
